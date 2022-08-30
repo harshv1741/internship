f35d9598fd29ec622d748835a732043600d6ec15
ardostree2001 <ardostree2001@outlook.com>
2022-08-30 18:12:32 +0530

    Added Some files

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+// Just a test for checking dbconnectivity
+$dbhost = '162.241.85.66:3306';
+// $portno = '3306';
+$dbname= 'abakujob_pallotti';
+$username = 'abakujob_pallotti';
+$password = 'Pallotti@1';
+
+$conn = mysqli_connect($dbhost,$username,$password,$dbname);
+
+if(mysqli_connect_errno())
+{
+  echo '<h3>failed to Connect: '.mysqli_connect_error(). '</h3>';
+  exit();
+}
+else{
+  echo '<h3>Database connected successfully!!!</h3>';
+}
+?>
