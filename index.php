0568a067ee137f06abc49c3f213f9dab58b377f7
ardostree2001 <ardostree2001@outlook.com>
2022-09-01 19:54:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
         <title>Document</title>
 </head>
 <body>
-        <h1>This is a text message for demonstration</h1>        
+        <h1>This is a text message for demonstration</h1>   
 </body>
 </html>
